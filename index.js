22a0876dccec0a9a8b73267ed9a2055fac6a7d34
Arthur <arthur.aba20@gmail.com>
2024-02-09 14:05:14 -0300

    Estrutura do Projeto em Node

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const router = express.Router();
+
+// Configura o Express para servir arquivos estáticos a partir do diretório 'public'
+app.use('/public', express.static('public'));
+
+router.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'pages/index.html'))
+});
+
+app.use('/', router);
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Servidor em Funcionamento!");
+});
